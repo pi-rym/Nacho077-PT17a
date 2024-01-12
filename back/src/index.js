81f9e451bed0ce613ffd834195999cc2c1da4d27
Ignacio <ignaciogimenez70@gmail.com>
2024-01-12 09:53:13 -0300

    homework promises finished

--- back/src/index.js
+++ back/src/index.js
@@ -1,6 +1,7 @@
 const http = require("http")
-const url2 = require("url")
+// const url2 = require("url")
 const data = require('./utils/data')
+const characterController = require("./controllers/getCharacters") // {getCharacterByID: () => {}}
 
 const PORT = 3001
 
@@ -12,15 +13,10 @@ http.createServer((req, res) => { // request, response
         // console.log(url2.parse(url)) Investigar
         const id = url.split("/").pop() // [rickandmorty, character, 321]
         if(Number(id) !== NaN){
-            const character = data.find(char => char.id == id) // filter(char => char.id !== id) // character[Number(id)]
-
-            if(character) {
-                return res.writeHead(200, {"Content-Type": "application/json"}) // text/plain
-                .end(JSON.stringify(character))
-            }
+            characterController(res, id) //.getCharacterByID(res, id)
+        } else {
+            res.writeHead(400, {"Content-Type": "text/plain"}).end("El id debe ser un número")
         }
-
-        res.writeHead(500, {"Content-Type": "text/plain"}).end("error")
     } else {
         res.writeHead(404, {"Content-Type": "text/plain"}).end("Not Found")
     }
